4b026bd4d54b3fc12956cc523d1e227d460a7951
E214595 <E214595@PC35958>
2017-03-13 09:34:18 +0100

    Added test.php

--- hsm/include/initialize.php
+++ hsm/include/initialize.php
@@ -1,2 +1,16 @@
 <?php
+class SETTING
+{
+	global enable_voucher;
+    
+	function init_setting()
+    {
+		$this->enable_voucher = 0;
+    }
+}
+
+global $mail_setting;
+
+$mail_setting = new SETTING();
+$mail_setting->init_setting();
 ?>
